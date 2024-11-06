57d8a6e8af259155efaa9ac4fce8a381b932607d
Luis Carlos <luiscqueiroz4@gmail.com>
2024-11-06 18:08:48 -0300

    Atualizacao das pastas de frontend (layout das paginas)e backend(bibliotecas do python)

---
+++ frontend/my-app/src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import Login from './components/login';  // Supondo que o login.jsx ou login.js seja o arquivo correto
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <Login />
+    </div>
+  );
+}
+
+export default App;
---
+++ frontend/my-app/src/components/login.js
@@ -0,0 +1,4 @@
+// login.js (caso queira um arquivo .js, pode importar o .jsx)
+import Login from './login.jsx';
+
+export default Login;
