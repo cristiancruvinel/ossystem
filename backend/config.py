c6399fca864faf218f4768328eb420610999baaf
Alexandre <alexandre.o.more@gmail.com>
2024-11-10 23:52:45 -0300

    ops

--- backend/config.py
+++ backend/config.py
@@ -3,7 +3,7 @@ from urllib.parse import quote_plus
 
 class Config:
     
-    password = quote_plus("LzrS*Bar3L@B4XG")
+    password = quote_plus("password here")
     SECRET_KEY = os.environ.get('SECRET_KEY', "secret_key")	
     SQLALCHEMY_DATABASE_URI = os.environ.get('SQLALCHEMY_DATABASE_URI',f"postgresql://postgres.kvtrgevagnpgpgjxznei:{password}@aws-0-us-east-1.pooler.supabase.com:6543/postgres")
     SQLALCHEMY_TRACK_MODIFICATIONS = False
