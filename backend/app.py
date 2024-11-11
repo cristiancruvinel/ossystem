0a098a5925e946ab67857f8f72ad67bcbd2c1c52
Alexandre <alexandre.o.more@gmail.com>
2024-11-10 23:49:01 -0300

    gitignore

--- backend/app.py
+++ backend/app.py
@@ -1,7 +1,8 @@
 from flask import Flask, jsonify
 from flask_cors import CORS
 from config import Config
-from models import cliente, produto, ordemDeServico, ordemProduto, usuario, init_db
+from models import db, init_db
+from routes import init_app
 
 
 app = Flask(__name__)
@@ -9,6 +10,8 @@ app.config.from_object(Config)
 CORS(app)
 
 init_db(app)
+
+init_app(app)
 
 if __name__ == '__main__':
     app.run(debug=True)
